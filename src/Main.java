61f30e57d63052331b90327e21a1d04223843c80
artur <artursuper10@gmail.com>
2022-10-05 21:53:44 +0300

    cicle1

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        // Циклы. Часть 1.  Домашнее задание -1. Задание 1
+        System.out.println("Циклы. Часть 1.  Домашнее задание -1. Задание 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        // Циклы. Часть 1.  Домашнее задание -1. Задание 2
+        System.out.println("Циклы. Часть 1.  Домашнее задание -1. Задание 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        // Циклы. Часть 1.  Домашнее задание -1. Задание 3
+        System.out.println("Циклы. Часть 1.  Домашнее задание -1. Задание 3");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println(i);
+        }
+        // Циклы. Часть 1.  Домашнее задание -1. Задание 4
+        System.out.println("Циклы. Часть 1.  Домашнее задание -1. Задание 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        // Циклы. Часть 1.  Домашнее задание -2. Задание 1
+        System.out.println("Циклы. Часть 1.  Домашнее задание -2. Задание 1");
+        for (int year = 1904; year <= 2096; year = year + 4) {
+            System.out.println(year);
+        }
+        // Циклы. Часть 1.  Домашнее задание -2. Задание 2
+        System.out.println("Циклы. Часть 1.  Домашнее задание -2. Задание 1");
+        for (int number = 7; number <= 98; number = number + 7) {
+            System.out.println(number);
+        }
+        // Циклы. Часть 1.  Домашнее задание -2. Задание 3
+        System.out.println("Циклы. Часть 1.  Домашнее задание -2. Задание 3");
+        for (int num = 1; num <= 512; num = num * 2) {
+            System.out.println(num);
+        }
+        // Циклы. Часть 1.  Домашнее задание -3. Задание 1
+        System.out.println("Циклы. Часть 1.  Домашнее задание -3. Задание 1");
+        int sum = 0;
+        for (int mouth = 1; mouth <= 12; mouth++) {
+            sum += 29_000;
+            System.out.println(" Mouth - " + mouth + " sum " + sum);
+        }
+        // Циклы. Часть 1.  Домашнее задание -3. Задание 2
+        System.out.println("Циклы. Часть 1.  Домашнее задание -3. Задание 2");
+
+    }
+}
